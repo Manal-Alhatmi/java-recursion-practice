8e8d040d490a9b1e40f3b282645894e549f0cd42
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-09-10 13:19:12 +0400

    Added StringReverser.java

---
+++ StringReverser.java
@@ -0,0 +1,21 @@
+public class StringReverser {
+
+    public static void main(String[] args) {
+        String original = "Hello World";
+        String reversed = "";
+
+        System.out.println("Reversing string using a loop...");
+
+        reversed = reverseStringRecursive(original);
+
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+
+   public static String reverseStringRecursive(String str) {
+        if (str.length() <= 1)
+            return str;
+
+        return reverseStringRecursive(str.substring(1))+ str.charAt(0);
+   }
+}
