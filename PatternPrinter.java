fa644a9faeb94583775897043a049a68fc0b51a6
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-09-10 13:28:28 +0400

    Added PatternPrinter.java

---
+++ PatternPrinter.java
@@ -0,0 +1,23 @@
+public class PatternPrinter {
+    public static void main(String[] args) {
+        int numRows = 5;
+        System.out.println("Printing a descending star pattern with nested loops...");
+        printPatternRecursive(numRows, numRows);
+
+    }
+
+    public static void printPatternRecursive(int rows, int cols) {
+        if (rows ==0) {
+            return;
+        }
+        if (cols > 0){
+            System.out.print("* ");
+            printPatternRecursive(rows, cols-1);
+        }
+        else {
+            System.out.println();
+            printPatternRecursive(rows-1, rows-1);
+        }
+
+    }
+}
